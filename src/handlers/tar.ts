29620ba3d857badab5aa4704b541efbe9dccf682
neko <neko681785@git.none>
2026-03-03 14:55:10 +0000

    fixed indenting in tar.ts

--- src/handlers/tar.ts
+++ src/handlers/tar.ts
@@ -17,29 +17,29 @@ class tarHandler implements FormatHandler {
 
   public name: string = "tar";
   public supportedFormats?: FileFormat[] = [
-      {
-        name: "Tape Archive",
-        format: "tar",
-        extension: "tar",
-        mime: "application/x-tar",
-        from: true,
-        to: true,
-        internal: "tar",
-        category: ["archive"],
-        lossless: true
-      },
-      CommonFormats.ZIP.builder("zip").allowFrom().allowTo().markLossless()
+    {
+      name: "Tape Archive",
+      format: "tar",
+      extension: "tar",
+      mime: "application/x-tar",
+      from: true,
+      to: true,
+      internal: "tar",
+      category: ["archive"],
+      lossless: true
+    },
+    CommonFormats.ZIP.builder("zip").allowFrom().allowTo().markLossless()
   ];
 
   public supportAnyInput: boolean = true;
 
   public ready: boolean = false;
 
-  async init () {
+  async init() {
     this.ready = true;
   }
 
-  async doConvert (
+  async doConvert(
     inputFiles: FileData[],
     inputFormat: FileFormat,
     outputFormat: FileFormat
@@ -47,68 +47,68 @@ class tarHandler implements FormatHandler {
     const outputFiles: FileData[] = [];
 
     if (inputFormat.internal == "zip" && outputFormat.internal == "tar") {
-        for (const inputFile of inputFiles) {
-            const zip = new JSZip();
-            await zip.loadAsync(inputFile.bytes);
+      for (const inputFile of inputFiles) {
+        const zip = new JSZip();
+        await zip.loadAsync(inputFile.bytes);
 
-            const archiveFiles: TarFileItem[] = [];
+        const archiveFiles: TarFileItem[] = [];
 
-            for (const [filename, zipEntry] of Object.entries(zip.files)) {
-                if (zipEntry.dir) {
-                    archiveFiles.push({ name: filename });
-                    continue;
-                }
-                const data = await zipEntry.async("uint8array");
+        for (const [filename, zipEntry] of Object.entries(zip.files)) {
+          if (zipEntry.dir) {
+            archiveFiles.push({ name: filename });
+            continue;
+          }
+          const data = await zipEntry.async("uint8array");
 
-                archiveFiles.push({
-                    name: filename,
-                    data,
-                    attrs: {
-                        mtime: zipEntry.date.getTime(),
-                        mode: zipEntry.unixPermissions?.toString(8)
-                    }
-                });
+          archiveFiles.push({
+            name: filename,
+            data,
+            attrs: {
+              mtime: zipEntry.date.getTime(),
+              mode: zipEntry.unixPermissions?.toString(8)
             }
-
-            const name = inputFile.name.replace(/\.zip$/i, ".tar");
-            const bytes = createTar(
-                archiveFiles,
-                { },
-            );
-
-            outputFiles.push({ bytes, name });
+          });
         }
-    } else if (inputFormat.internal == "tar" && outputFormat.internal == "zip") {
-        for (const inputFile of inputFiles) {
-            const files = parseTar(inputFile.bytes);
 
-            const zip = new JSZip();
-
-            for (const file of files) {
-                const date = file.attrs?.mtime ? new Date(file.attrs?.mtime * 1000) : undefined;
-                const unixPermissions = file.attrs?.mode;
-
-                if (!file.data) {
-                    zip.file(file.name, null, { dir: true, date, unixPermissions });
-                } else {
-                    zip.file(file.name, file.data, { date, unixPermissions });
-                }
-            }
-
-            const bytes = await zip.generateAsync({ type: "uint8array" });
-
-            const name = inputFile.name.replace(/\.tar$/i, ".zip");
-            outputFiles.push({ bytes, name });
-        }
-    } else if (outputFormat.internal == "tar") {
+        const name = inputFile.name.replace(/\.zip$/i, ".tar");
         const bytes = createTar(
-            inputFiles.map(file => ({ name: file.name, data: file.bytes })),
-            { },
+          archiveFiles,
+          {},
         );
-        const name = inputFiles.length == 1 ? inputFiles[0].name + ".tar" : "archive.tar";
-        outputFiles.push({ bytes, name })
+
+        outputFiles.push({ bytes, name });
+      }
+    } else if (inputFormat.internal == "tar" && outputFormat.internal == "zip") {
+      for (const inputFile of inputFiles) {
+        const files = parseTar(inputFile.bytes);
+
+        const zip = new JSZip();
+
+        for (const file of files) {
+          const date = file.attrs?.mtime ? new Date(file.attrs?.mtime * 1000) : undefined;
+          const unixPermissions = file.attrs?.mode;
+
+          if (!file.data) {
+            zip.file(file.name, null, { dir: true, date, unixPermissions });
+          } else {
+            zip.file(file.name, file.data, { date, unixPermissions });
+          }
+        }
+
+        const bytes = await zip.generateAsync({ type: "uint8array" });
+
+        const name = inputFile.name.replace(/\.tar$/i, ".zip");
+        outputFiles.push({ bytes, name });
+      }
+    } else if (outputFormat.internal == "tar") {
+      const bytes = createTar(
+        inputFiles.map(file => ({ name: file.name, data: file.bytes })),
+        {},
+      );
+      const name = inputFiles.length == 1 ? inputFiles[0].name + ".tar" : "archive.tar";
+      outputFiles.push({ bytes, name })
     } else {
-        throw "tarHandler cannot process this conversion";
+      throw "tarHandler cannot process this conversion";
     }
 
     return outputFiles;
