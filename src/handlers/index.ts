afea80368ea3c383ddf535a9cc0019b3efaa9ca6
Omega Fallon <36779526+omega-fallon@users.noreply.github.com>
2026-03-03 15:19:15 -0500

    Support for "Over the Air bitmap" format (#488)
    * OTA decoding
    
    * Proper file extension
    
    * Round-trip conversion now possible.
    
    ---------
    
    Co-authored-by: p2r3 <41925384+p2r3@users.noreply.github.com>

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -55,6 +55,7 @@ import n64romHandler from "./n64rom.ts";
 import vexflowHandler from "./vexflow.ts";
 import toonHandler from "./toon.ts";
 import rpgmvpHandler from "./rpgmvp.ts";
+import otaHandler from "./ota.ts";
 import comicsHandler from "./comics.ts";
 import terrariaWldHandler from "./terrariawld.ts";
 
@@ -117,6 +118,7 @@ try { handlers.push(new n64romHandler()) } catch (_) { };
 try { handlers.push(new vexflowHandler()) } catch (_) { };
 try { handlers.push(new toonHandler()) } catch (_) { };
 try { handlers.push(new rpgmvpHandler()) } catch (_) { };
+try { handlers.push(new otaHandler()) } catch (_) { };
 try { handlers.push(new comicsHandler()) } catch (_) { };
 try { handlers.push(new terrariaWldHandler()) } catch (_) { };
 
